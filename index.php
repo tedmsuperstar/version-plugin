85c431fa482ef7af65281286f39693933a0f12af
Ted Metzger <tedmetzger@Teds-MacBook-Pro.local>
2021-08-17 12:01:02 -0700

    added index file

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+Plugin Name: Version Plugin
+Plugin URI: http://legitimatebusinessmodel.com/
+Description: Version 1.0 for testing builds/deploys with a plugin as a submodule.
+Author: Ted Metzger
+Version: 1.0
+Author URI: http://legitimatebusinessmodel.com/
+*/
+
+
+
